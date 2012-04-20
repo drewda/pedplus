6db9db55fe63336942f977443b81352ad3d41ec5
Drew Dara-Abrams <drew@s3sol.com>
2012-04-20 11:59:05 -0700

    PEDPLUS-58: clear old ModelJob's when a new one has finished

--- app/models/model_job.rb
+++ app/models/model_job.rb
@@ -1,3 +1,14 @@
 class ModelJob < ActiveRecord::Base
   belongs_to :project
+  before_save :delete_old_model_jobs
+  def delete_old_model_jobs
+    # delete all the other old model jobs that have been run
+    # for this project and are the same kind of model as this
+    # new one
+    project.model_jobs.each do |model_job|
+      if model_job.kind == self.kind
+        model_job.destroy unless model_job == self
+      end
+    end
+  end
 end
